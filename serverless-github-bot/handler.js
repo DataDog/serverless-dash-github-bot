10aa9d05c02aa02495e2b2b2007b804df8adc792
AJ Stuyvenberg <astuyve@gmail.com>
2022-09-07 15:57:05 -0400

    sns not sqs

--- serverless-github-bot/handler.js
+++ serverless-github-bot/handler.js
@@ -4,9 +4,12 @@ AWS.config.update({region: 'us-east-1'});
 const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
 module.exports.webhook = async (event) => {
-  await sqs.sendMessage({
-    MessageBody: event.body,
-    QueueUrl: process.env.SQS_QUEUE
+  const body = JSON.parse(event.body)
+  body.ajTimestamp = Date.now()
+
+  await sns.publish({
+    Message: JSON.stringify(body),
+    TopicArn: process.env.TOPIC_ARN
   }).promise()
   return {
     statusCode: 200,
@@ -14,7 +17,13 @@ module.exports.webhook = async (event) => {
     {
       "content-type": "application/json"
     },
-    body: JSON.stringify({status: 'pushed', body})
+    body: JSON.stringify({
+      keyOne: 'foobar',
+      myObject: {
+        anotherKey: ['array', 'of', 'values']
+      },
+      val: null
+    })
   }
 }
 
